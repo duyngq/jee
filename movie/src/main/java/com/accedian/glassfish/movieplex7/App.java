92bb108209011db5bc93dab79f6afe1b0e78a5fa
nguyenquocduy <nguyenquocduy@tma.com.vn>
2016-03-14 16:05:49 +0700

    Init Maven Project

---
+++ movie/src/main/java/com/accedian/glassfish/movieplex7/App.java
@@ -0,0 +1,13 @@
+package com.accedian.glassfish.movieplex7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
